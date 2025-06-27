cffadc5c9d2d6dd9fd93a02887e02e21de065739
WebWeaverMohit <mohitshrivastava0000@gmail.com>
2025-06-27 22:50:54 +0530

    changed the ui theme

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
+require('dotenv').config(); // ✅ This stays here
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const expressSession = require('express-session')
+const expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var commentRouter = require('./routes/comment')
-var postsRouter = require('./routes/posts')
+var commentRouter = require('./routes/comment');
+var postsRouter = require('./routes/posts');
 const passport = require('passport');
 
-const connectDB = require('./config/db'); 
+const connectDB = require('./config/db');
 const MongoStore = require('connect-mongo');
 
 connectDB();
@@ -29,10 +30,10 @@ app.use(expressSession({
   secret: "yehhh"
 }));
 
-app.use(passport.initialize())
-app.use(passport.session())
-passport.serializeUser(usersRouter.serializeUser())
-passport.deserializeUser(usersRouter.deserializeUser())
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(usersRouter.serializeUser());
+passport.deserializeUser(usersRouter.deserializeUser());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -42,8 +43,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/posts/:id/comments', commentRouter)
-app.use('/posts', postsRouter)
+app.use('/posts/:id/comments', commentRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -52,11 +53,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
